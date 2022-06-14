c400cc6085d74a92b1ea7a65c1688ab7907bf2e7
Shubham Sharma <shubham.sharma12@nagarro.com>
2022-06-14 14:56:24 +0530

    Add TestNG in Runner

--- src/test/java/Runners/SearchCarsRunner.java
+++ src/test/java/Runners/SearchCarsRunner.java
@@ -1,12 +1,22 @@
 package Runners;
-import org.junit.runner.RunWith;
+import org.testng.annotations.BeforeClass;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
 
-@RunWith(Cucumber.class)
 @CucumberOptions( features= "C:./src/test/resources/features/SearchCars.feature",
 				  glue= "stepdefinitions")
-public class SearchCarsRunner {
+
+
+public class SearchCarsRunner extends AbstractTestNGCucumberTests {
+	
+	 private TestNGCucumberRunner testNGCucumberRunner;
+
+
+	    @BeforeClass(alwaysRun = true)
+	    public void setUpCucumber() {
+	         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+	    }
 
 }
